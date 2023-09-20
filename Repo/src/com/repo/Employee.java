f61f2ab1acc9f0480ac24bfeed576a7b6eb762bc
Araddhya <1941012880.araddhya@gmail.com>
2023-09-20 12:09:08 +0530

    commit

---
+++ Repo/src/com/repo/Employee.java
@@ -0,0 +1,15 @@
+package com.repo;
+
+public class Employee {
+	boolean Even(int n) {
+		if(n%2!=0)
+			//System.out.println("even");
+		return false;
+		return false;
+			
+	}
+ public static void main(String[] args) {
+	 Employee e=new Employee();
+	 System.out.println(e.Even(4));
+ }
+}
